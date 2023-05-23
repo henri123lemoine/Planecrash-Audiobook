9706820c8c2ac845ce29b394b41b0a9b14a69048
henri123lemoine <henri123lemoine@gmail.com>
2023-05-22 23:48:54 -0400

    Updated gitignore and settings

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,108 @@
 BOARD_URL = "https://glowfic.com/board_sections/703"
 
 JSON_PATH = "data/threads JSON"
+BOARDS_PATH = "data/board_chunks"
+
+narrator_voice = "fletcher"
+keltham_voice = "flynn"
+
+carissa_voice = "victor"
+asmodia_voice = "Daisy"
+abrogail_voice = "Aurora"
+pilar_voice = "hipolito"
+ione_voice = "Lottie"
+aspexia_voice = "spencer"
+jacint_voice = ""
+korva_voice = ""
+perenza_voice = ""
+derrina_voice = "gabriel"
+lady_avaricia_voice = ""
+nefreti_voice = ""
+
+ferrer_voice = "abram"
+merenre_voice = ""
+fe_anar_voice = ""
+khemet_3_voice = ""
+broom_voice = ""
+ri_dul_voice = ""
+terrified_prisoner_voice = ""
+befutig_voice = ""
+
+curse_of_laughter_voice = ""
+cayden_cailean_voice = ""
+dispater_voice = ""
+indapatta_voice = ""
+asmodeus_voice = ""
+irori_voice = ""
+question_mark_voice = ""
+city_of_brass_voice = ""
+abadar_voice = "donovan"
+zakiya_voice = ""
+otolmens_voice = ""
+milani_voice = ""
+iomedae_voice = ""
+zon_kuthon_voice = ""
+nethys_voice = ""
+gorthoklek_voice = ""
+pit_fiend_voice = ""
+
+glosialabolas_voice = ""
+taravind_voice = ""
+hadrian_voice = ""
+absalom_voice = ""
+sajvara_voice = ""
+tarnish_voice = ""
+ostenso_voice = ""
+phistophilus_voice = ""
+ismat_alnahhat_voice = ""
+ex_criminal_wizard_student_voice = ""
+rosal_monserrat_voice = ""
+elysium_voice = ""
+hemaka_voice = ""
+ioun_stone_voice = ""
+class_chatter_voice = ""
+mayor_of_senara_voice = ""
+minor_character_voice = ""
+road_voice = ""
+infernal_fortress_voice = ""
+roc_de_castell_voice = ""
+olegario_voice = ""
+carmin_isandre_voice = ""
+calistria_voice = ""
+alexite_voice = ""
+terrified_newly_assigned_assistant_enchanter_voice = ""
+alexandre_esquerra_voice = ""
+chelish_noble_voice = ""
+thellim_voice = ""
+shelyn_voice = ""
+isfahel_kelesh_voice = ""
+oppara_voice = ""
+tiny_translucent_sphere_voice = ""
+athpechya_voice = ""
+erecura_voice = ""
+other_women_voice = ""
+mora_of_maragall_voice = ""
+anashala_voice = ""
+akkakarasot_voice = ""
+tira_shabbar_voice = ""
+nanhsa_voice = ""
+modavian_voice = ""
+willa_voice = ""
+merrin_voice = "anny"
+character_1_voice = "dylan"
+character_2_voice = "clark"
 
 VOICE_DICT = {
-    "Keltham": (keltham_voice:="Keltham"),
+    "Narrator": narrator_voice,
+    "Security": narrator_voice,
+    "dath ilan": narrator_voice,
+    "Cheliax": narrator_voice,
+    "Project Lawful": narrator_voice,
+    "Greater Reality": narrator_voice,
+    "AAAAAAAAAAA (Security)": narrator_voice,
+    "Classroom": narrator_voice,
+
+    "Keltham": keltham_voice,
     "Keltham+": keltham_voice,
     "Keltham v3": keltham_voice,
     "dath Keltham??": keltham_voice,
@@ -14,108 +113,99 @@ VOICE_DICT = {
     "Carissa's Model of Keltham": keltham_voice,
     "A Somewhat Dubious Model of Keltham": keltham_voice,
     
-    "Carissa Sevar": (carissa_voice:="Carissa"),
+    "Carissa Sevar": carissa_voice,
 
-    "Narrator": (narrator_voice:="Narrator"),
-    "Security": narrator_voice,
-    "dath ilan": narrator_voice,
-    "Cheliax": narrator_voice,
-    "Project Lawful": narrator_voice,
-    "Greater Reality": narrator_voice,
-    "AAAAAAAAAAA (Security)": narrator_voice,
-    "Classroom": narrator_voice,
-
-    "Asmodia": (asmodia_voice:="Asmodia"),
+    "Asmodia": asmodia_voice,
     "Greater Asmodia": asmodia_voice,
-    "Abrogail Thrune II": (abrogail_voice:="Abrogail"),
+    "Abrogail Thrune II": abrogail_voice,
     "Abrogail": abrogail_voice,
-    "Pilar ": (pilar_voice:="Pilar"),
+    "Pilar ": pilar_voice,
     "Pilar Pineda": pilar_voice,
-    "Ione Sala": (ione_voice:="Ione"),
-    "Aspexia Rugatonn": (aspexia_voice:="Aspexia"),
+    "Ione Sala": ione_voice,
+    "Aspexia Rugatonn": aspexia_voice,
     "Carissa's Model of Aspexia": aspexia_voice,
-    "Jacint Subirachs": (jacint_voice:="Jacint"),
-    "Korva Tallandria": (korva_voice:="Korva"),
-    "Peranza": (perenza_voice:="Peranza"),
+    "Jacint Subirachs": jacint_voice,
+    "Korva Tallandria": korva_voice,
+    "Peranza": perenza_voice,
     "Peranza of Civilization": perenza_voice,
     "AlterPeranza": perenza_voice,
-    "Derrina": (derrina_voice:="Derrina"),
-    "Lady Eulàlia Avaricia de Seguer": (lady_avaricia_voice:="Lady Avaricia"),
+    "Derrina": derrina_voice,
+    "Lady Eulàlia Avaricia de Seguer": lady_avaricia_voice,
     "Lady Eul\u00e0lia Avaricia de Seguer": lady_avaricia_voice,
-    "Nefreti Clepati": (nefreti_voice:="Nefreti"),
+    "Nefreti Clepati": nefreti_voice,
 
-    "Ferrer Maillol": (ferrer_voice:="Ferrer"),
+    "Ferrer Maillol": ferrer_voice,
     "Maillol's Remains": ferrer_voice,
-    "Merenre": (merenre_voice:="Merenre"),
-    "Prince Fe-Anar": (fe_anar_voice:="Fe-Anar"),
-    "Ruby Prince Khemet III": (khemet_3_voice:="Khemet III"),
-    "Halfling slave #958245 \"Broom\"": (broom_voice:="Broom"),
-    "Ri-Dul": (ri_dul_voice:="Ri-Dul"),
-    "Incredibly Terrified Prisoner": (terrified_prisoner_voice:="Terrified Prisoner"),
-    "Befutig Safiza Uj-alet": (befutig_voice:="Befutig"),
+    "Merenre": merenre_voice,
+    "Prince Fe-Anar": fe_anar_voice,
+    "Ruby Prince Khemet III": khemet_3_voice,
+    "Halfling slave #958245 \"Broom\"": broom_voice,
+    "Ri-Dul": ri_dul_voice,
+    "Incredibly Terrified Prisoner": terrified_prisoner_voice,
+    "Befutig Safiza Uj-alet": befutig_voice,
 
-    "Curse of Laughter": (curse_of_laughter_voice:="Curse of Laughter"),
-    "Cayden Cailean": (cayden_cailean_voice:="Cayden Cailean"),
-    "Dispater": (dispater_voice:="Dispater"),
-    "Indapatta": (indapatta_voice:="Indapatta"),
-    "Asmodeus": (asmodeus_voice:="Asmodeus"),
-    "Irori": (irori_voice:="Irori"),
-    "???": (question_mark_voice:="???"),
-    "City of Brass": (city_of_brass_voice:="City of Brass"),
-    "Abadar": (abadar_voice:="Abadar"),
-    "Zakiya": (zakiya_voice:="Zakiya"),
-    "Otolmens": (otolmens_voice:="Otolmens"),
-    "Milani": (milani_voice:="Milani"),
-    "Iomedae": (iomedae_voice:="Iomedae"),
+    "Curse of Laughter": curse_of_laughter_voice,
+    "Cayden Cailean": cayden_cailean_voice,
+    "Dispater": dispater_voice,
+    "Indapatta": indapatta_voice,
+    "Asmodeus": asmodeus_voice,
+    "Irori": irori_voice,
+    "???": question_mark_voice,
+    "City of Brass": city_of_brass_voice,
+    "Abadar": abadar_voice,
+    "Zakiya": zakiya_voice,
+    "Otolmens": otolmens_voice,
+    "Milani": milani_voice,
+    "Iomedae": iomedae_voice,
     "Greater Iomedae": iomedae_voice,
-    "Zon-Kuthon": (zon_kuthon_voice:="Zon-Kuthon"),
-    "Nethys": (nethys_voice:="Nethys"),
-    "Gorthoklek": (gorthoklek_voice:="Gorthoklek"),
-    "Pit Fiend": (pit_fiend_voice:="Pit Fiend"),
+    "Zon-Kuthon": zon_kuthon_voice,
+    "Nethys": nethys_voice,
+    "Gorthoklek": gorthoklek_voice,
+    "Pit Fiend": pit_fiend_voice,
 
-    "Glosialabolas": (glosialabolas_voice:="Glosialabolas"),
-    "Taravind": (taravind_voice:="Taravind"),
-    "Hadrian": (hadrian_voice:="Hadrian"),
-    "Absalom": (absalom_voice:="Absalom"),
-    "Sajvara": (sajvara_voice:="Sajvara"),
-    "Tarnish": (tarnish_voice:="Tarnish"),
-    "Ostenso": (ostenso_voice:="Ostenso"),
-    "Phistophilus": (phistophilus_voice:="Phistophilus"),
-    "Ismat Alnahhat": (ismat_alnahhat_voice:="Ismat Alnahhat"),
-    "Ex-Criminal Wizard Student Who Received His Wish To Have His Criminality Beaten Out Of Him": (ex_criminal_wizard_student_voice:="Ex-Criminal Wizard Student"),
-    "Rosal Monserrat": (rosal_monserrat_voice:="Rosal Monserrat"),
-    "Elysium": (elysium_voice:="Elysium"),
-    "Hemaka": (hemaka_voice:="Hemaka"),
-    "literal fucking IOUN stone": (ioun_stone_voice:="Ioun Stone"),
-    "Class Chatter": (class_chatter_voice:="Class Chatter"),
-    "Mayor of Senara": (mayor_of_senara_voice:="Mayor of Senara"),
-    "Minor Character": (minor_character_voice:="Minor Character"),
-    "Road": (road_voice:="Road"),
-    "Infernal Fortress": (infernal_fortress_voice:="Infernal Fortress"),
-    "Roc de Castell": (roc_de_castell_voice:="Roc de Castell"),
-    "Olegario": (olegario_voice:="Olegario"),
-    "Carmin Isandre": (carmin_isandre_voice:="Carmin Isandre"),
-    "Calistria": (calistria_voice:="Calistria"),
-    "Alexite Gellius Rutilus Thrune": (alexite_voice:="Alexite"),
-    "Terrified Newly-Assigned Assistant Enchanter Who Wasn't Briefed Very Much And Is Now Certain He's Going To Die Horribly": (terrified_newly_assigned_assistant_enchanter_voice:="Terrified Newly-Assigned Assistant Enchanter"),
-    "Alexandre Esquerra": (alexandre_esquerra_voice:="Alexandre Esquerra"),
-    "Chelish Noble": (chelish_noble_voice:="Chelish Noble"),
-    "Thellim": (thellim_voice:="Thellim"),
-    "Shelyn": (shelyn_voice:="Shelyn"),
-    "Isfahel, Kelesh": (isfahel_kelesh_voice:="Isfahel, Kelesh"),
-    "Oppara": (oppara_voice:="Oppara"),
-    "Tiny Translucent Sphere": (tiny_translucent_sphere_voice:="Tiny Translucent Sphere"),
-    "Athpechya": (athpechya_voice:="Athpechya"),
-    "Erecura": (erecura_voice:="Erecura"),
-    "Other Women Have Wanted To Strangle Boyfriends But Never With This Much Justification Quantitatively Speaking": (other_women_voice:="Other Women"),
-    "Mora of Maragall": (mora_of_maragall_voice:="Mora of Maragall"),
-    "Anashala": (anashala_voice:="Anashala"),
-    "Akkakarasot": (akkakarasot_voice:="Akkakarasot"),
-    "Tira Shabbar": (tira_shabbar_voice:="Tira"),
-    "Nanhsa": (nanhsa_voice:="Nanhsa"),
-    "Modavian": (modavian_voice:="Modavian"),
-    "Willa Shilira": (willa_voice:="Willa"),
-    "Merrin": (merrin_voice:="Merrin"),
-    "Character 1": (character_1_voice:="C1"),
-    "Character 2": (character_2_voice:="C2")
+    "Glosialabolas": glosialabolas_voice,
+    "Taravind": taravind_voice,
+    "Hadrian": hadrian_voice,
+    "Absalom": absalom_voice,
+    "Sajvara": sajvara_voice,
+    "Tarnish": tarnish_voice,
+    "Ostenso": ostenso_voice,
+    "Phistophilus": phistophilus_voice,
+    "Ismat Alnahhat": ismat_alnahhat_voice,
+    "Ex-Criminal Wizard Student Who Received His Wish To Have His Criminality Beaten Out Of Him": ex_criminal_wizard_student_voice,
+    "Rosal Monserrat": rosal_monserrat_voice,
+    "Elysium": elysium_voice,
+    "Hemaka": hemaka_voice,
+    "literal fucking IOUN stone": ioun_stone_voice,
+    "Class Chatter": class_chatter_voice,
+    "Mayor of Senara": mayor_of_senara_voice,
+    "Minor Character": minor_character_voice,
+    "Road": road_voice,
+    "Infernal Fortress": infernal_fortress_voice,
+    "Roc de Castell": roc_de_castell_voice,
+    "Olegario": olegario_voice,
+    "Carmin Isandre": carmin_isandre_voice,
+    "Calistria": calistria_voice,
+    "Alexite Gellius Rutilus Thrune": alexite_voice,
+    "Terrified Newly-Assigned Assistant Enchanter Who Wasn't Briefed Very Much And Is Now Certain He's Going To Die Horribly": terrified_newly_assigned_assistant_enchanter_voice,
+    "Alexandre Esquerra": alexandre_esquerra_voice,
+    "Chelish Noble": chelish_noble_voice,
+    "Thellim": thellim_voice,
+    "Shelyn": shelyn_voice,
+    "Isfahel, Kelesh": isfahel_kelesh_voice,
+    "Oppara": oppara_voice,
+    "Tiny Translucent Sphere": tiny_translucent_sphere_voice,
+    "Athpechya": athpechya_voice,
+    "Erecura": erecura_voice,
+    "Other Women Have Wanted To Strangle Boyfriends But Never With This Much Justification Quantitatively Speaking": other_women_voice,
+    "Mora of Maragall": mora_of_maragall_voice,
+    "Anashala": anashala_voice,
+    "Akkakarasot": akkakarasot_voice,
+    "Tira Shabbar": tira_shabbar_voice,
+    "Nanhsa": nanhsa_voice,
+    "Modavian": modavian_voice,
+    "Willa Shilira": willa_voice,
+    "Merrin": merrin_voice,
+    "Character 1": character_1_voice,
+    "Character 2": character_2_voice
 }
